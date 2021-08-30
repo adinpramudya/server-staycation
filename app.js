a901cf6fb4ea552d1044c8356be222a4a3a03047
Adinda Pramudya Ramadhan <61746276+adinpramudya@users.noreply.github.com>
2021-08-30 15:07:21 +0700

    cors enabled

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const cors = require('cors')
 
 //import koneksi
 const mongoose = require('mongoose');
@@ -46,6 +47,7 @@ app.use('/users', usersRouter);
 // admin
 app.use('/admin', adminRouter);
 app.use('/api/v1/member', apiRouter);
+app.use(cors());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
