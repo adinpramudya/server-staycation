21bef85bf1f7f7ab06683faa5606ec7ceecd3751
Adinda Pramudya Ramadhan <61746276+adinpramudya@users.noreply.github.com>
2021-08-30 16:39:27 +0700

    cors enabled edit

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(cors());
 app.use(methodOverride('_method'));
 app.use(session({
   secret: 'keyboard cat',
@@ -47,7 +48,6 @@ app.use('/users', usersRouter);
 // admin
 app.use('/admin', adminRouter);
 app.use('/api/v1/member', apiRouter);
-app.use(cors());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
